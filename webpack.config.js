e60e914693190cd0400bd11f06bba18b42d9dd5d
vlad <vladlisin2@gmail.com>
2023-09-18 16:53:50 +0700

    fast

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "public", "index.html"),
+            template: path.join(__dirname, "index.html"),
             name: "index.html",
             inject: false,
         }),
